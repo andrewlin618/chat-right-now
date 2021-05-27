64a8ea58e6295bd86fd471d635bbfd3316450a15
ANDREW LIN <andrewlin618@gmail.com>
2021-05-26 17:58:00 -0700

    added auto scroll

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
+import { makeStyles } from "@material-ui/core/styles";
 import moment from "moment";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "70vh",
+    overflowY: "scroll"
+  }
+}));
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const classes = useStyles();
+  const bottomRef = useRef();
+
+  const scrollToBottom = () => {
+    bottomRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  },[])
+
+  useEffect(() => {
+    scrollToBottom();
+  },[messages.length])
 
   return (
-    <Box>
+    <Box className={classes.root}>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
@@ -17,6 +42,7 @@ const Messages = (props) => {
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
       })}
+      <div ref={bottomRef} />
     </Box>
   );
 };
