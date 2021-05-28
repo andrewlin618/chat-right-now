3345b6ab0245b9a54fccbf3834fa9d4f11ef5d60
ANDREW LIN <andrewlin618@gmail.com>
2021-05-28 09:33:27 -0700

    moved messages ordering logic to backend

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -62,9 +62,6 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
-    for (const conversation of data) {
-      conversation.messages.reverse();
-    }
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
